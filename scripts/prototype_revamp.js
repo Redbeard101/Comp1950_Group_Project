02ece20d694ed71d3df569ebcdbc387b3772c29c
Jonathan Worth <jonathan.v.worth@gmail.com>
2018-11-27 20:33:40 -0800

    Slide toggle

--- scripts/prototype_revamp.js
+++ scripts/prototype_revamp.js
@@ -1,5 +1,20 @@
 $(document).ready(function(){
     $("h2").click(function(){
-        $(this).next('.contblock').slideToggle(450, "linear");
+        $(this).next('.contblock').slideToggle().toggleClass('opened');
+        var isVisible = $(this).next('.contblock').is( ".opened" );
+        if (isVisible === true ){
+            $(this).css('background-color', '#6afca2');
+            $(this).hover(function(){
+                $(this).css('background-color', '#8bf9b6');
+                }, function(){
+                $(this).css('background-color', '#6afca2');});
+        }
+        else {
+            $(this).css('background-color', '#3971c6');     
+            $(this).hover(function(){
+                $(this).css('background-color', '#9ec9ff');
+                }, function(){
+                $(this).css('background-color', '#3971c6');});   
+        }    
 	});		
 });
