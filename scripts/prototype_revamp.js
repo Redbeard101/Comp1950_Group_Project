d1401501564e36f8e2164f64deab9b3c898c03fb
Jonathan Worth <jonathan.v.worth@gmail.com>
2018-11-27 21:15:05 -0800

    Expanded

--- scripts/prototype_revamp.js
+++ scripts/prototype_revamp.js
@@ -1,3 +1,8 @@
+//Author: Jonathan Worth A00949372
+//Expanding/Contracting functionality
+//Nov 22nd, 2018
+//COMP 1950 
+
 $(document).ready(function(){
     $("h2").click(function(){
         $(this).next('.contblock').slideToggle().toggleClass('opened');
@@ -16,5 +21,34 @@ $(document).ready(function(){
                 }, function(){
                 $(this).css('background-color', '#3971c6');});   
         }    
-	});		
+    });//End h2.click
+    
+    $("#expand").click(function(){
+        $(".contblock").each(function(){
+            var isVisible = $(this).is( ".opened" );
+            if (isVisible === false ){
+                $(this).slideToggle().toggleClass('opened');
+            }
+        });//End each .contblock
+        $("h2").each(function(){
+            $(this).css('background-color', '#6afca2');
+            $(this).hover(function(){ $(this).css('background-color', '#8bf9b6');}, 
+            function(){ $(this).css('background-color', '#6afca2');});
+        });//End each h2
+        
+    });//End expand click
+
+    $("#collapse").click(function(){
+        $(".contblock").each(function(){
+            var isVisible = $(this).is( ".opened" );
+            if (isVisible === true ){
+                $(this).slideToggle().toggleClass('opened');
+            }
+        });//End each .contblock
+        $("h2").each(function(){
+            $(this).css('background-color', '#3971c6');
+            $(this).hover(function(){ $(this).css('background-color', '#9ec9ff');}, 
+            function(){ $(this).css('background-color', '#3971c6');});
+        });//End each h2
+    });//End collapse click
 });
